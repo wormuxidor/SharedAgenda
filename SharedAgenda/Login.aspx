07aeb892f73de88429a216843d53d4d3241e06e8
i017 <i017@DESKTOP-H8INSQU>
2018-04-24 10:45:37 +0200

    Login-logik hinzugefügt

--- SharedAgenda/Login.aspx
+++ SharedAgenda/Login.aspx
@@ -1,15 +1,39 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="SharedAgenda.Login" %>
-
-<!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title>Login</title>
-</head>
-<body>
-    <form id="form1" runat="server">
-        <div>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/general.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="SharedAgenda.Login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <script type="text/javascript">
+    onload=function(){
+    var e=document.getElementById("refreshed");
+    if(e.value=="no")e.value="yes";
+    else{e.value="no";location.reload();}
+    }
+    </script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <input type="hidden" id="refreshed" value="no" >
+    <div class="contentWhole">
+    <div class="contentCentered">
+        <div class="contentHeader">
+            <h1 class="loginHeader">Login</h1>
         </div>
-    </form>
-</body>
-</html>
+        <div class="formular1">
+            <asp:Label runat="server" Text="Username:" CssClass="sansseriflabel"></asp:Label>
+            <asp:Label runat="server" Text="Password:" CssClass="sansseriflabel"></asp:Label>
+        </div>
+        <div class="formular2">
+            <asp:TextBox runat="server" ID="usernameBox" CssClass="formularTextBox"></asp:TextBox>
+            <asp:RequiredFieldValidator runat="server" ID="FieldValidator1" ControlToValidate="usernameBox" ValidationGroup="ValidLogin">
+            </asp:RequiredFieldValidator>
+            <asp:TextBox runat="server" ID="passwordBox" EnableViewState="false" TextMode="Password" CssClass="formularTextBox"></asp:TextBox>
+            <asp:RequiredFieldValidator runat="server" ID="FieldValidator2" ControlToValidate="passwordBox" ValidationGroup="ValidLogin">
+            </asp:RequiredFieldValidator>
+        </div>
+        <br />
+            <asp:Button runat="server" ID="button1" OnClick="button1_Click" Text="Anmelden" CssClass="floatRightButton" 
+                CausesValidation="true" ValidationGroup="ValidLogin" ValidateRequestMode="Enabled" />
+        <br />
+        <div class="clear">
+            <asp:Label ID="lblmsgM" runat="server"></asp:Label>
+        </div>
+        </div>
+    </div>
+</asp:Content>
--- SharedAgenda/Login.aspx.cs
+++ SharedAgenda/Login.aspx.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,9 +15,112 @@ namespace SharedAgenda
 {
     public partial class Login : System.Web.UI.Page
     {
+        public string connectionString = ConfigurationManager.ConnectionStrings["ConStr"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void checkDetails()
+        {
+            Boolean isRight = false;
+            String user = usernameBox.Text;
+            String password = passwordBox.Text;
+
+            Boolean isAdmin = false;
+            Boolean isErweitert = false;
+
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            MySqlCommand command = new MySqlCommand("getPwhash", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            command.Parameters.Add("inKuerzel", MySqlDbType.VarChar).Value = user;
+            conn.Open();
+
+            using (MySqlDataAdapter sda = new MySqlDataAdapter(command))
+            {
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                DataRow row = dt.Rows[0];
+                string passwordhash = Convert.ToString(row.ItemArray[0]);
+                long salt = Convert.ToInt64(row.ItemArray[1]);
+                isErweitert = Convert.ToBoolean(row.ItemArray[2]);
+                isAdmin = Convert.ToBoolean(row.ItemArray[3]);
+                if (passwordhash == this.sha256_hash(password + salt))
+                {
+                    isRight = true;
+                }
+                conn.Close();
+
+            }
+            if (isRight == true)
+            {
+                MySqlConnection conn2 = new MySqlConnection(connectionString);
+                string connectionString2 = String.Format("UPDATE Users SET isLoggedIn = 1 WHERE Kuerzel = '{0}';", user);
+                MySqlCommand command2 = new MySqlCommand(connectionString2, conn2);
+                conn2.Open();
+                int response = command2.ExecuteNonQuery();
+                conn2.Close();
+                if (response == 1)
+                {
+                    if (isAdmin)
+                    {
+                        usernameBox.Enabled = false;
+                        passwordBox.Enabled = false;
+                        Response.AppendHeader("Refresh", String.Format("1;url=./admin.aspx?id={0}", user));
+                    }
+                    else if (isErweitert)
+                    {
+                        usernameBox.Enabled = false;
+                        passwordBox.Enabled = false;
+                        Response.AppendHeader("Refresh", String.Format("1;url=./reservierenErweitert.aspx?id={0}", user));
+                    }
+                    else
+                    {
+                        usernameBox.Enabled = false;
+                        passwordBox.Enabled = false;
+                        Response.AppendHeader("Refresh", String.Format("1;url=./reservieren.aspx?id={0}", user));
+                    }
+                }
+                else
+                {
+                    Label lbl = (Label)this.Master.FindControl("lblmsgMaster");
+                    lbl.BackColor = Color.Red;
+                    lbl.ForeColor = Color.White;
+                    lbl.Text = " Es ist ein Fehler aufgetreten.....";
+                }
+
+
+            }
+            else
+            {
+                Label lbl = (Label)this.Master.FindControl("lblmsgMaster");
+                lbl.BackColor = Color.Red;
+                lbl.ForeColor = Color.White;
+                lbl.Text = " Username oder Passwort ist falsch.....";
+            }
+        }
+
+        private String sha256_hash(String value)
+        {
+            StringBuilder Sb = new StringBuilder();
+
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                Byte[] result = hash.ComputeHash(enc.GetBytes(value));
+
+                foreach (Byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+
+            return Sb.ToString();
+        }
+
+        protected void button1_Click(object sender, EventArgs e)
+        {
+            this.checkDetails();
         }
     }
 }
