d06f6e0a9a2d14531c10a6d0298285d77d222a75
Main <Main@DESKTOP-F1ITJPK>
2018-05-11 17:57:32 +0200

    icons incremented + style fixes

--- SharedAgenda/Shared_Agenda.aspx.designer.cs
+++ SharedAgenda/Shared_Agenda.aspx.designer.cs
@@ -73,6 +73,6 @@ namespace SharedAgenda {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button log_out_button;
+        protected global::System.Web.UI.WebControls.LinkButton log_out_button;
     }
 }
