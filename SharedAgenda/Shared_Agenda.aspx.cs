cb4fa1bd73f9cafa78c1f0146352df9fb3e07d36
Daniel Lukas <daniel.lukas@edubs.ch>
2018-05-17 10:18:09 +0200

    GetDatesOfWeek

--- SharedAgenda/Shared_Agenda.aspx.cs
+++ SharedAgenda/Shared_Agenda.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Security;
@@ -10,7 +13,10 @@ namespace SharedAgenda
 {
     public partial class Shared_Agenda : System.Web.UI.Page
     {
+
+        public string connectionString = ConfigurationManager.ConnectionStrings["ConStr"].ConnectionString;
         String[] userData;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             this.userData = (String[]) Session["userData"];
@@ -22,11 +28,34 @@ namespace SharedAgenda
             {
                 newEventButton.Attributes.Add("style","display:none");
             }
+
+            Get_Date();
+        }
+
+        protected void Get_Date()
+        {
+            SqlConnection conn = new SqlConnection(connectionString); //Connectionstring erstellen
+
+            SqlCommand cmd = new SqlCommand("Get_Eintrag", conn)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+            };
+
+            cmd.Parameters.Add("@Board", SqlDbType.NVarChar).Value = class_list.SelectedItem.Text;
+            cmd.Parameters.Add("@WochenStart", SqlDbType.DateTime).Value = GetDaysOfWeek(year, int.Parse(week_selection.SelectedItem.Text) - 1); // UNFUNCTIONAL Montag der Woche von Woche und Jahr ableiten.
+            cmd.Parameters.Add("@WochenEnde", SqlDbType.DateTime).Value = GetDaysOfWeek(year, int.Parse(week_selection.SelectedItem.Text)); // UNFUNCTIONAL Montag der nächsten Woche von Woche und Jahr ableiten
+
+            cmd.Connection = conn;
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
         }
 
         protected void week_selection_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            Get_Date();
         }
 
         protected void log_out_button_Click(object sender, EventArgs e)
@@ -43,6 +72,26 @@ namespace SharedAgenda
             {
                 Response.Redirect("NewEvent.aspx");
             }*/
+        }
+
+        protected DateTime GetDaysOfWeek(int year, int Woche)
+        {
+            
+            DateTime date = new DateTime(year, 1, 1);
+            DateTime newDate = date;
+
+            while (Convert.ToString(newDate.DayOfWeek) != "Monday")
+            {
+                newDate = newDate.AddDays(-1);
+            }
+
+            //herausfinden des Montages aus der Woche des 1.Januar
+
+            newDate = newDate.AddDays(7 * Woche);
+
+            // Woche verrechnen
+
+            return newDate;
         }
     }
 }
